4e615848183267c90d8014efb2c6c534ab2090e4
gabrielMina <gabrielmina118@gmail.com>
2021-01-06 16:22:28 -0300

    inserindo verificação de status da requisicao

--- js/buscarPacientes.js
+++ js/buscarPacientes.js
@@ -7,12 +7,18 @@ botaoPaciente.addEventListener("click",function(){
     xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load",function(){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
+        if(xhr.status == 200){
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+            console.log(xhr.status);
+        }else{
+            console.log(xhr.status);
+        }
+        
     });
 
     xhr.send();
