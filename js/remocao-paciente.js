01abdd669a814b0b61fe50f30446bb367a81d2aa
gabrielMina <gabrielmina118@gmail.com>
2021-01-06 13:47:01 -0300

    inserindo a filtragem ,utilizando classe CSS

--- js/remocao-paciente.js
+++ js/remocao-paciente.js
@@ -12,6 +12,7 @@ var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick",function(e){
     var alvo = e.target;
     var superiorAlvo = alvo.parentNode;
+    console.log(superiorAlvo);
     superiorAlvo.classList.add("fadeOut"); // Esmaecer o tempo ate a opacidade = 0 ;
   
     //funcao de tempo no javascript
